80e01d55939c85d72686fe4429dbab09570dd16c
thomasmburke <tburke@bu.edu>
2020-04-27 23:22:45 -0700

    added sample json data for testing

---
+++ ios/ios/Models/UserData.swift
@@ -0,0 +1,14 @@
+//
+//  UserData.swift
+//  ios
+//
+//  Created by Tom Burke on 4/27/20.
+//  Copyright © 2020 Bruin Fitness. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+final class UserData: ObservableObject {
+    @Published var workouts = workoutData
+}
