dcab574654f21de28da395bd6ddfd092e732b971
thomasmburke <tburke@bu.edu>
2020-04-28 17:29:40 -0700

    adding nice to haves / bugs

---
+++ ios/ios/ProgrammingViews/DeleteMe.swift
@@ -0,0 +1,21 @@
+//
+//  DeleteMe.swift
+//  ios
+//
+//  Created by Tom Burke on 4/28/20.
+//  Copyright © 2020 Bruin Fitness. All rights reserved.
+//
+
+import SwiftUI
+
+struct DeleteMe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DeleteMe_Previews: PreviewProvider {
+    static var previews: some View {
+        DeleteMe()
+    }
+}
