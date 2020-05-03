0f2f5853413477c08dd4571ee389a8a3d1f1ce6a
thomasmburke <tburke@bu.edu>
2020-05-02 22:53:23 -0700

    creating schedule filter horizontal ScrollView

---
+++ ios/ios/ScheduleViews/ScheduleFilterListView.swift
@@ -0,0 +1,21 @@
+//
+//  ScheduleFilterListView.swift
+//  ios
+//
+//  Created by Tom Burke on 5/2/20.
+//  Copyright © 2020 Bruin Fitness. All rights reserved.
+//
+
+import SwiftUI
+
+struct ScheduleFilterListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScheduleFilterListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleFilterListView()
+    }
+}
